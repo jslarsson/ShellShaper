e59774d66599b8bfd7344eb8762f7e606c7f35d7
jslarsson <60740363+jslarsson@users.noreply.github.com>
2020-03-13 11:59:20 +0100

    Added file
    ShellShaper for circliptic aperture without bands.

---
+++ ShellShaper.m
@@ -0,0 +1,19 @@
+% Path to folder containing photos and functions
+addpath .\ShellsToShape
+addpath .\ShellModelFunctions
+
+% Use all .jpg and .jpeg files in the photo folder
+contents = dir('ShellsToShape\*.jp*g');
+
+% Save in the following table and folder
+saveas = '.\ShapedShells\parameters.txt';
+saveimagefolder = '.\ShapedShells';
+
+
+% Go through these shell images.
+startNumber = 1;
+lastNumber = numel(contents);
+
+
+% Run program
+ShellShaperProgram(contents, saveas, saveimagefolder, startNumber, lastNumber)
